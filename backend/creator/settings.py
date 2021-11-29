04de9a4fd1fd28d0197a3f2ecaa9eb3335033108
Alex Serebryakov <avserebryakov@innotech.com>
2021-11-29 18:15:45 +0300

    Перенес хранение картинок в aws-s3-bucket

--- backend/creator/settings.py
+++ backend/creator/settings.py
@@ -20,6 +20,9 @@ env = environ.Env(
     EMAIL_HOST_USER=(str, "someuser@gmail.com"),
     EMAIL_HOST_PASSWORD=(str, "password"),
     PUBLIC_URL=(str, "http://localhost/"),
+    AWS_STORAGE_BUCKET_NAME=(str, "AWS_STORAGE_BUCKET_NAME"),
+    AWS_S3_ACCESS_KEY_ID=(str, "AWS_S3_ACCESS_KEY_ID"),
+    AWS_S3_SECRET_ACCESS_KEY=(str, "AWS_S3_SECRET_ACCESS_KEY"),
 )
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -184,3 +187,8 @@ EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
 ADMIN_EMPTY_VALUE_DISPLAY = "-пусто-"
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_ACCESS_KEY_ID = env("AWS_S3_ACCESS_KEY_ID")
+AWS_S3_SECRET_ACCESS_KEY = env("AWS_S3_SECRET_ACCESS_KEY")
