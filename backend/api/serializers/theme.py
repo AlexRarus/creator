56708af26933811b297b7c90e5f3ab6d6bab80de
Alex Serebryakov <avserebryakov@innotech.com>
2021-12-07 18:10:03 +0300

    update to_internal_value ThemeSerializer method

--- backend/api/serializers/theme.py
+++ backend/api/serializers/theme.py
@@ -23,16 +23,16 @@ class ThemeSerializer(serializers.ModelSerializer):
 
     def to_internal_value(self, data):
         # вызывается перед созданием/обновлением модели
-        background_outer = data.pop("background")
+        background = data.pop("background")
         internal = super().to_internal_value(data)
-        internal["background_primary"] = background_outer.get("primary")
-        internal["background_secondary"] = background_outer.get("secondary")
+        for attr, value in background.items():
+            setattr(internal, f"background_{attr}", value)
+
         return internal
 
     def update(self, instance, validated_data):
         for attr, value in validated_data.items():
-            new_value = value if value is not None else getattr(instance, attr)
-            setattr(instance, attr, new_value)
+            setattr(instance, attr, value)
         instance.save()
 
         return instance
