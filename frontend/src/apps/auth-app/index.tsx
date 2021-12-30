8ed90988826ddebaca8606098af1a29c539aad69
Viktor V. Serebryakov <vserebryakov@tsconsulting.com>
2021-12-30 14:19:57 +0300

    fix height auth page

--- frontend/src/apps/auth-app/index.tsx
+++ frontend/src/apps/auth-app/index.tsx
@@ -1,22 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Router from 'src/router';
 import routes from 'src/router/routes/auth-routes';
-// import { useAppTypeContext } from 'src/providers/app-type-provider';
-// import { isMobile } from 'react-device-detect';
+import { useAppTypeContext } from 'src/providers/app-type-provider';
+import { isMobile } from 'react-device-detect';
 
 import { AuthLayout } from './layout';
 
 const AuthApp = () => {
-  // const { setAppType } = useAppTypeContext();
-  //
-  // useEffect(() => {
-  //   if (isMobile) {
-  //     // блокировка скролла для нормальной работы dnd
-  //     setAppType('app');
-  //   }
-  //
-  //   return () => setAppType('web');
-  // }, [isMobile]);
+  const { setAppType } = useAppTypeContext();
+
+  useEffect(() => {
+    if (isMobile) {
+      // блокировка скролла для нормальной работы dnd
+      setAppType('app');
+      window.scrollTo(0, 0);
+    }
+
+    return () => setAppType('web');
+  }, [isMobile]);
   return (
     <AuthLayout>
       <Router routes={routes} />
