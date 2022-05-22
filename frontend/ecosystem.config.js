2cba39706faadc830e1edcb97142a2144fe25edb
Alex Serebryakov <avserebryakov@innotech.com>
2022-05-22 18:30:14 +0300

    добавил сбор логов с сервера для ssr

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -2,6 +2,11 @@ module.exports = {
   apps : [{
     name: 'ssr',
     script: './build-ssr/ssr.js',
-    watch: '.'
+    watch: '.',
+    autorestart: true,
+    out_file: './ssr-logs/output.log', // логи
+    error_file: './ssr-logs/error.log', // ошибки
+    log_file: './ssr-logs/log.log', // и логи и ошибки в одном файле
+    log_date_format: 'YYYY-MM-DD HH:mm Z',
   }]
 };
