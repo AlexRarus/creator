d0d5610bd5d194cf8d28a1f0495f3cafc250716a
Alex Serebryakov <avserebryakov@innotech.com>
2022-05-27 12:05:25 +0300

    add bcc for ActivationEmail and PasswordReset

--- backend/users/email.py
+++ backend/users/email.py
@@ -1,9 +1,26 @@
+from django.conf import settings
 from djoser import email
 
 
 class PasswordReset(email.PasswordResetEmail):
     template_name = "email/password_reset.html"
 
+    def send(self, to, *args, **kwargs):
+        bcc = [settings.DEFAULT_FROM_EMAIL]
+        try:
+            super().send(to, *args, **kwargs, bcc=bcc)
+        except Exception:
+            print(f"Couldn't send mail to {to}")
+            raise
+
 
 class ActivationEmail(email.ActivationEmail):
     template_name = "email/activation.html"
+
+    def send(self, to, *args, **kwargs):
+        bcc = [settings.DEFAULT_FROM_EMAIL]
+        try:
+            super().send(to, *args, **kwargs, bcc=bcc)
+        except Exception:
+            print(f"Couldn't send mail to {to}")
+            raise
